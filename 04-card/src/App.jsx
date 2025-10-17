2c4d0b87d409b1f923db5b032d681f56c92ed8ae
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-10-17 17:38:05 +0600

    learn (map and forech)

--- 04-card/src/App.jsx
+++ 04-card/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Bookmark } from "lucide-react";
+import Card from "./Components/Card";
+import User from "./Components/User";
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+const arr = [10,20,30,40,509,600]
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="parent">
+  {arr.map (function(elem){
+    return <h1>{elem/2}</h1>
+  })}
+   
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ 04-card/src/Components/Card.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Bookmark } from "lucide-react";
+
+const Card = () => {
+  return (
+      <div className="card">
+ <div>
+         <div className="top">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg"
+            alt=""
+          />
+          <button>
+            Save <Bookmark size={13} />
+          </button>
+        </div>
+        <div className="center">
+          <h3>
+            Amazon <span>5 days ago</span>
+          </h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div className="tag">
+            <h4>Part time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+ </div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Dhaka, Bangladesh</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+  )
+}
+
+export default Card
