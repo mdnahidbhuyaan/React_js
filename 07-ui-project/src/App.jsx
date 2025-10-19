4a3a9a9cfc63f36f709a092901db1d4dfc1e241d
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-10-19 22:53:08 +0600

    erro burro SSS

---
+++ 07-ui-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Section1 from './components/section-1/Section1'
+import Section2 from './components/section-2/Section2'
+
+const App = () => {
+  return (
+    <div className=''>
+    <Section1/>
+    <Section2/>
+    <Section2/>
+    <Section2/>
+    </div>
+  )
+}
+
+export default App
