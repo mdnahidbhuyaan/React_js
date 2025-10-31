cd0ed34b5485bf1eb3a1ad6118ffad5cc8f85feb
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-10-31 11:45:27 +0600

    useState

--- 16-use-effect/src/App.jsx
+++ 16-use-effect/src/App.jsx
@@ -1,13 +1,84 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [a , setA] = useState(0)
+  const [b , setB] = useState(0)
+function aChanging (){
+  console.log("aChanging is running...")
+}
+function bChanging (){
+  console.log("bChanging is running...")
+}
+  useEffect(function(){
+    aChanging()
+    // console.log("useEffect is running...")
+  },[a])
 
-  
+  useEffect(function(){
+    bChanging()
+    // console.log("useEffect is running...")
+  },[b])
   return (
     <div>
-      
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button onClick={()=>{
+        setA(a+1)
+      }}>ChangeA</button>
+      <button
+      onClick={()=>{
+        setB(b-1)
+      }}
+      >ChangeB</button>
+
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [data, setData] = useState([])
+//   const GetData =async () => {
+// const response = await axios.get("https://picsum.photos/v2/list")
+// setData(response.data)
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={GetData}>GetData</button>
+//       {data.map(function(elem,idx){
+//         return  <h3 >Hello {elem.author}{idx}</h3>
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-
-import axios from "axios";
 import React, { useState } from "react";
-
+import axios from "axios";
 const App = () => {
-  const [data, setData] = useState([])
-  const GetData =async () => {
-const response = await axios.get("https://picsum.photos/v2/list")
-setData(response.data)
+  const [userdata, setUserData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=2&limit=30"
+    );
+    setUserData(response.data);
   };
 
+  useState(() => {
+    getData();
+  }, []);
+  let printUserData = "No user Available";
+  if (userdata.length > 0) {
+    printUserData = userdata.map(function (elem, idx) {
+      return (
+        <div key={idx}>
+          <a href={elem.url} target="_blank">
+            <div className="h-40 w-44 overflow-hidden mt-10">
+              <img
+                src={elem.download_url}
+                alt=""
+                className="h-full w-full  object-cover rounded-xl"
+              />
+            </div>
+            <h2 className="font-bold text-lg">{elem.author}</h2>
+          </a>
+        </div>
+      );
+    });
+  }
   return (
-    <div>
-      <button onClick={GetData}>GetData</button>
-      {data.map(function(elem,idx){
-        return  <h3 >Hello {elem.author}{idx}</h3>
-      })}
+    <div className="p-10  overflow-auto h-screen">
+      <div className="flex flex-wrap gap-4 p-2">{printUserData}</div>
+      <div className="flex justify-center gap-5 items-center p-4">
+        <button className="bg-amber-400 text-black rounded text-sm cursor-pointer active:scale-95 px-4 py-1 font-semibold ">Prev</button>
+        <button className="bg-amber-400 text-black rounded text-sm cursor-pointer active:scale-95 px-4 py-1 font-semibold ">Next</button>
+      </div>
     </div>
   );
 };
