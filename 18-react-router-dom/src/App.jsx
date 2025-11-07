b8d0c0b71861b12ae79ebd9886d0424905d17107
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-11-07 19:33:20 +0600

    react router dom complate

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About'
+import Contact from './pages/Contact.jsx'
 const App = () => {
   return (
     <div>
-      Hello Home
+      <div>
+        <h3>Nahdi</h3>
+        <div className='nav'>
+          <a href="/"> Home</a>
+          <a href="/about"> about</a>
+          <a href="/contact"> contact</a>
+        </div>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+
+      
+      </Routes>
+        
     </div>
   )
 }
